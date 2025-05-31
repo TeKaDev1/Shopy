3005830d513467e6b408e946cdba6fa8bc0fdc36
Mohamed <itzhapy@gmail.com>
2025-05-31 07:57:41 -0700

    final

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage, connectStorageEmulator } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAwvH0kCjpr5H2Wlqwpva0PC0vjxeIM46o",
-  authDomain: "tekadev1.firebaseapp.com",
-  databaseURL: "https://tekadev1-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "tekadev1",
-  storageBucket: "tekadev1.appspot.com",
-  messagingSenderId: "1003700748004",
-  appId: "1:1003700748004:web:ff23010162612fbef7f6f1"
+  apiKey: "AIzaSyBXhnT5AWMmJnQMDRFTlE5hdtKbjB4nxNw",
+  authDomain: "dkhil-32644.firebaseapp.com",
+  projectId: "dkhil-32644",
+  storageBucket: "dkhil-32644.firebasestorage.app",
+  messagingSenderId: "37336137805",
+  appId: "1:37336137805:web:b4a3eae4650a7e87405c04",
+  measurementId: "G-92BRXN0F8Z"
 };
 
 // Initialize Firebase with error handling
