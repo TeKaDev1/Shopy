6ed9de5f105678bfa9041405687f042ba2a30ff1
Mohamed <itzhapy@gmail.com>
2025-06-01 02:28:58 -0700

    Br

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,23 +3,22 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
+
+  // your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAwvH0kCjpr5H2Wlqwpva0PC0vjxeIM46o",
   authDomain: "tekadev1.firebaseapp.com",
   databaseURL: "https://tekadev1-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "tekadev1",
-  storageBucket: "tekadev1.appspot.com",
+  storageBucket: "tekadev1.firebasestorage.app",
   messagingSenderId: "1003700748004",
   appId: "1:1003700748004:web:ff23010162612fbef7f6f1"
 };
 
-try {
-  const firebaseApp = initializeApp(firebaseConfig);
-  const auth = getAuth(firebaseApp);
-  const database = getDatabase(firebaseApp);
-  const storage = getStorage(firebaseApp);
 
-  export { firebaseApp, auth, database, storage };
-} catch (error) {
-  console.error('Error initializing Firebase:', error);
-}
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+const storage = getStorage(app);
+
+export { app, auth, database, storage };
